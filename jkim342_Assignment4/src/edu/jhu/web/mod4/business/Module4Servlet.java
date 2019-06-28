b0af5a230eee7f5fc5781b66e71b9cb0cb9bb7e6
Justin Kim <justinjaekim@gmail.com>
2019-06-28 06:44:45 -0400

    assignment4 added

---
+++ jkim342_Assignment4/src/edu/jhu/web/mod4/business/Module4Servlet.java
@@ -0,0 +1,42 @@
+package edu.jhu.web.mod4.business;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Module4Servlet
+ */
+@WebServlet("/Module4Servlet")
+public class Module4Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Module4Servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String url = "cost.jsp";
+		
+	}
+
+}

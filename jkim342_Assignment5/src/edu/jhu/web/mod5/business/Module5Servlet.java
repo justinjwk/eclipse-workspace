094364cb6aad93a7be4918129630086c64ca2a8f
Justin Kim <justinjaekim@gmail.com>
2019-07-03 07:44:19 -0400

    initial assignment5 is added

---
+++ jkim342_Assignment5/src/edu/jhu/web/mod5/business/Module5Servlet.java
@@ -0,0 +1,59 @@
+package edu.jhu.web.mod5.business;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Module4Servlet
+ */
+@WebServlet("/Module4Servlet")
+public class Module5Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Module5Servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String url = "cost.jsp";
+
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String status = request.getParameter("employmentstatus");
+		String[] courses = request.getParameterValues("course");		
+
+		User user = new User(name, email, status, courses);
+		request.setAttribute("user", user);
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher("/cost.jsp");
+
+		dispatcher.forward(request, response);
+
+		
+	}
+
+}
